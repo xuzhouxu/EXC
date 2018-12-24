ca4036064bcb974de9996dce3e0aaa29ebec34bb
韩意谦 <33115108+HYQ1999@users.noreply.github.com>
2018-12-24 15:30:31 +0800

    添加loginC

---
+++ EMC/EMC/Loginn/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  EMC
+//
+//  Created by 韩意谦 on 2018/12/24.
+//  Copyright © 2018 韩意谦. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
