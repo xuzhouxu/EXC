914002524899005ef978b127b9a5eb5cb3ec391b
韩意谦 <33115108+HYQ1999@users.noreply.github.com>
2018-12-27 16:28:54 +0800

    登录页面布局无约束版

--- EMC/EMC/Loginn/LoginViewController.swift
+++ EMC/EMC/Loginn/LoginViewController.swift
@@ -10,13 +10,34 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var btnSure: UIButton!
+    @IBOutlet weak var vPassword: UIView!
+    @IBOutlet weak var vUser: UIView!
+    @IBOutlet weak var TPassword: UITextField!
+    @IBOutlet weak var TUserName: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        TUserName.borderStyle = UITextField.BorderStyle.none
+        TUserName.clearButtonMode = .always
+        //TUserName.returnKeyType = UIReturnKeyType.done
+        TPassword.borderStyle = UITextField.BorderStyle.none
+        TPassword.clearButtonMode = .always
+        TPassword.isSecureTextEntry = true
+        //TPassword.returnKeyType = UIReturnKeyType.done
+        vUser.layer.borderWidth = 0.5
+        vUser.layer.borderColor = UIColor.lightGray.cgColor
+        vUser.backgroundColor = UIColor.white
+        vPassword.layer.borderWidth = 0.5
+        vPassword.layer.borderColor = UIColor.lightGray.cgColor
+        vPassword.backgroundColor = UIColor.white
+        btnSure.layer.cornerRadius = 5
+        }
+    @IBAction func viewClick(_ sender: Any) {
+        TUserName.resignFirstResponder()
     }
-    
-
+    @IBAction func ReturnBack(_ sender: Any) {
+        TPassword.resignFirstResponder()
+    }
     /*
     // MARK: - Navigation
 
